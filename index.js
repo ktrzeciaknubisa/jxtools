27a098bcff49ab861abc428908500a29f51d32ee
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-10-02 18:36:12 +0200

    commit

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 // Copyright & License details are available under JXCORE_LICENSE file
 
+var fs = require('fs');
+var path = require('path');
+var common = require('./lib/common.js');
+
+
+for(var o in common) {
+  if (common.hasOwnProperty(o))
+    exports[o] = common[o];
+}
+
 if (typeof jxcore === 'undefined')
   jxcore = {utils: require('./lib/jx/_jx_utils')};
+
+
+exports.fs = require('./lib/tools/fs');
+exports.http = require('./lib/tools/http');
+
+
 
