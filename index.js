c61ad57529a8195b6bc670887ec2a1339994ab95
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-10-06 23:27:12 +0200

    multiple updates

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var path = require('path');
 var common = require('./lib/common.js');
 
 
-for(var o in common) {
+for (var o in common) {
   if (common.hasOwnProperty(o))
     exports[o] = common[o];
 }
@@ -14,8 +14,10 @@ if (typeof jxcore === 'undefined')
   jxcore = {utils: require('./lib/jx/_jx_utils')};
 
 
+exports.console = require('./lib/tools/console');
 exports.fs = require('./lib/tools/fs');
 exports.http = require('./lib/tools/http');
+exports.zip = require('./lib/tools/zip');
 
 
 
